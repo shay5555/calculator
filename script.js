615e9084611ef11d90a2c629c04e48974869ac77
shay5555 <62674745+shay5555@users.noreply.github.com>
2022-12-27 18:44:17 +0200

    Block user from entering two consecutive non-number values

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 //get output screen text and turn it to an array
-const outputScreenText = document.querySelector('.screen-text');
+const outputScreenText    = document.querySelector('.screen-text');
 
-const numberButtons = document.querySelectorAll('button.number');
-const operatorButtons = document.querySelectorAll('button.operator');
-const clearButton = document.getElementById('clear');
-const getResultButton = document.getElementById('get-result');
-const signChangeButton = document.getElementById('sign-change');
-const decimalPointButton = document.getElementById('decimal-point');
-const printToScreenButton = document.querySelectorAll('.buttons .write-to-screen')
-const allButtons = document.querySelectorAll('.buttons button');
+const numberButtons       = document.querySelectorAll('button.number');
+const operatorButtons     = document.querySelectorAll('button.operator');
+const printToScreenButton = document.querySelectorAll('.buttons .write-to-screen');
+const allButtons          = document.querySelectorAll('.buttons button');
+const clearButton         = document.getElementById('clear');
+const getResultButton     = document.getElementById('get-result');
+const signChangeButton    = document.getElementById('sign-change');
+const decimalPointButton  = document.getElementById('decimal-point');
 
+//get last character of the user input
+let lastChar = outputScreenText.textContent.charAt(outputScreenText.textContent.length-1);
 
 printToScreenButton.forEach(e => {
     e.addEventListener('click', function () {
@@ -22,7 +24,13 @@ clearButton.addEventListener('click', function () {
 })
 
 function updateScreenText (newChar) {
+    //don't allow user to input 2 non-number items in a row
+    if (!parseInt(lastChar) && !parseInt(newChar)) {
+        return;
+    }
+
     outputScreenText.textContent += `${newChar}`;
+    lastChar = newChar;
 }
 
 function operateAdd (x, y) {
@@ -53,9 +61,8 @@ function operate (x, y, operator) {
             return operateDivide(x, y);
         default:
             alert("Invalid operator");
-            return null;
+            return null;    
     }
 }
 
-
-console.log(numberButtons);
+console.log();
